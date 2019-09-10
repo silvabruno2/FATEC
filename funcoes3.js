0954b67b8edb51a6538c0ceb0b51d55e6fa7d5b7
BRUNO OLIVEIRA SILVA <1090481823004@fatec.local>
2019-09-10 10:44:25 -0300

    Adicionado exercicios 7 e 8

--- funcoes3.js
+++ funcoes3.js
@@ -121,3 +121,67 @@ function ex6() {
     }
     alert(`Total a vista: ${totalVista} \n Total a Prazo: ${totalPrazo} \n Total Geral: ${totalVista + totalPrazo} \n Primeira parcela do valor a prazo: ${(totalPrazo / 3).toFixed(2)}`)
 }
+
+function ex7() {
+    var aux1 = 0
+    var aux2 = 0
+    var aux3 = 0
+    var alt = 0
+    var opcao
+    do {
+        var idade = Number(prompt("Digite a idade:"))
+        var altura = Number(prompt("Digite a altura"))
+        var peso =  Number(prompt("Digite o peso"))
+        var contador
+        if (idade > 50){
+            aux1 ++
+        }
+        else if (idade >= 10 && idade <= 20){
+            aux2 = aux2 + altura
+            alt ++
+        }
+        else if (peso < 40){
+            aux3 = aux3++
+        }
+        var opcao = prompt("Deseja continuar ? (S/N)").toUpperCase();
+    }
+    while (opcao != "N")
+    alert(`Quantidade de pessoas com idade maior que 50 anos: ${aux1} \n A média das alturas de pessoas de 10 a 20 anos: ${(aux2/alt).toFixed(2)} \n Porcentagem de pessoas com menos de 40 kg: ${aux3/contador*100}`)
+} 
+
+function ex8() {
+    var aux1 = 0
+    var aux2 = 0
+    var aux3 = 0
+    var alt = 0
+    var opcao
+    var pes = 0
+    var aux4 = 0
+    var contador
+    do {
+        var idade = Number(prompt("Digite a idade:"))
+        var altura = Number(prompt("Digite a altura em cm:"))
+        var peso =  Number(prompt("Digite o peso"))
+        var olho = prompt("Digite A para olho azul, C para castanho, P para preto e V para verde").toUpperCase()
+        var cabelo = prompt("Digite P para preto, C para castanho, L para loiro e R para ruivo").toUpperCase()
+        contador++
+        if (idade > 50 && peso < 60){
+            aux1 ++
+        }
+        else if (altura <= 150){
+            aux2++
+            idade = idade++        
+        }
+        else if (olho = "A"){
+            aux3 = aux3++
+        }
+        else if (cabelo = "R")
+            if (olho != "A"){
+            aux4 ++
+        }
+        var opcao = prompt("Deseja continuar ? (S/N)").toUpperCase();
+    }
+    while (opcao != "N")
+    alert(`Quantidade de pessoas com idade maior que 50 anos e menos de 60Kg: ${aux1} \n A média de pessoas com menos de 1,50m : ${(idade/aux2).toFixed(2)} 
+    \n Porcentagem de pessoas com olho azul: ${aux3/contador*100} \n Quantidade de pessoas de olhos azuis que não são ruivas: ${aux4}`)
+}
